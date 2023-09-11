dacd309522aced51fc306786b6bffe332af53c61
Luan <luan6992@outlook.com>
2023-09-10 22:35:51 -0300

    feat: creatin and uploading ex03

---
+++ MusicaEPlaylist/src/Playlist.java
@@ -0,0 +1,11 @@
+public class Playlist {
+    public static void main(String[] args) {
+        Musica shineOnYouCrazyDiamond = new Musica();
+        shineOnYouCrazyDiamond.setNome("Shine On You Crazy Diamond");
+        shineOnYouCrazyDiamond.setArtista("Pink Floyd");
+        shineOnYouCrazyDiamond.setDuracao(27);
+        shineOnYouCrazyDiamond.setGenero("Progressivo/Rock Psicodélico");
+
+        shineOnYouCrazyDiamond.imprimir();
+    }
+}
