8430efb1c3a7b701f947e0bdcddce12bda861af2
Luan <luan6992@outlook.com>
2023-09-10 22:28:48 -0300

    feat: creating and uploading ex02

---
+++ CarroEConcessionaria/src/Concessionaria.java
@@ -0,0 +1,11 @@
+public class Concessionaria {
+    public static void main(String[] args) {
+        Carro corolla = new Carro();
+        corolla.setModelo("Toyota Corolla");
+        corolla.setAno(2022);
+        corolla.setPreco(100000000);
+        corolla.setMarca("Toyota");
+
+        corolla.imprimir();
+    }
+}
